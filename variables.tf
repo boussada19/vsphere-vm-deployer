b3f0b73c0018e60b68743bf50e9338b2e3497d66
boussada19 <124534419+boussada19@users.noreply.github.com>
2025-07-14 23:29:51 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,18 @@
-variable "vsphere_user" {}
-variable "vsphere_password" { sensitive = true }
-variable "vsphere_server" {}
+variable "vsphere_user" {
+  type        = string
+  description = "Nom d'utilisateur vSphere"
+  default     = "administrator@vsphere.local"
+}
 
-variable "datacenter" {}
-variable "datastore" {}
-variable "host_name" {}
-variable "network" {}
-variable "template_name" {}
+variable "vsphere_password" {
+  type        = string
+  description = "Mot de passe vSphere"
+  sensitive   = true
+  default     = "y#1>hwAsr%r,sx1"
+}
 
-variable "vm_name" {}
-variable "vm_cpu" {}
-variable "vm_memory" {}
-variable "vm_disk" {}
-
-variable "vm_ip" {}
-variable "vm_gateway" {}
-variable "dns_server" {}
-variable "admin_password" {}
+variable "vsphere_server" {
+  type        = string
+  description = "Adresse IP ou nom DNS du vCenter Server"
+  default     = "10.0.1.50"
+}
